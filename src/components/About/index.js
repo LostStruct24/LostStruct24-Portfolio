80816e1a3b6da8adb6016939c97e046da5e97cb9
N <75640078+N12345678987654321@users.noreply.github.com>
2021-04-05 00:17:24 +0400

    Portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { Cow } from '../CssCow/Cow';
 import { SiGithub } from 'react-icons/si';
 import { AboutContainer, AboutBg, AboutContent, AboutH1, AboutP, AboutBtnWrapper, AboutBtn } from './AboutElements';
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 550
+        }
+      }
+    }
+  }
+  
 const About = () => {
     return (
         <AboutContainer id="home">
+            <Particles className='particles'
+            params={particlesOptions} />
             <AboutBg></AboutBg>
             <AboutContent>
                 <Cow />
